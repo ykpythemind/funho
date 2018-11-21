ab933be2013eca6f738f9ad08474a95964096695
Yukito Ito <yukibukiyou@gmail.com>
2018-11-21 18:21:36 +0900

    charset

--- migrations/20181116174834-create_users.sql
+++ migrations/20181116174834-create_users.sql
@@ -4,7 +4,7 @@ CREATE TABLE users (
   id integer PRIMARY KEY AUTO_INCREMENT,
   name varchar(128) NOT NULL UNIQUE,
   password varchar(128) NOT NULL
-);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE chats (
   id integer PRIMARY KEY AUTO_INCREMENT,
@@ -14,7 +14,7 @@ CREATE TABLE chats (
   created_at datetime,
   FOREIGN KEY (user_id) REFERENCES users(id),
   FOREIGN KEY (room_user_id) REFERENCES users(id)
-);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- +migrate Down
 DROP TABLE chats;
